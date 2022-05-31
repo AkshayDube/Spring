23b8de23fecfffa043dc35d3af44c8e18209b5dd
AkshayDube <dubeyakshay09@gmail.com>
2022-05-31 18:56:26 +0530

    Name change for Application Context(Spring Container) File

--- spring-demo-1/src/main/java/com/Spring/demo/App.java
+++ spring-demo-1/src/main/java/com/Spring/demo/App.java
@@ -6,7 +6,7 @@ public class App {
 
 	public static void main(String[] args) {
 		//Create A Spring Container
-		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//Retreive Bean from Spring Container
 		Coach obj=context.getBean("coach",Coach.class);
